46de7a0f4f0c18042043ff37821c0de23f583e0d
Gustavo Manoel Elias <gustavomanoelelias13@gmail.com>
2026-03-09 18:15:43 -0300

    Initial commit - estrutura base do FestaGestor

---
+++ src/main/java/br/com/festagestor/FestagestorApplication.java
@@ -0,0 +1,13 @@
+package br.com.festagestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestagestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestagestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/festagestor/FestagestorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.festagestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestagestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
