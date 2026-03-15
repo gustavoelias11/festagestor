ba24d660cfeafbf5eec47736a29ea753d190defb
Gustavo Manoel Elias <gustavomanoelelias13@gmail.com>
2026-03-15 08:59:38 -0300

    chore(setup): cria template de configuracao do banco de dados

--- src/main/java/br/com/festagestor/model/Brinquedo.java
+++ src/main/java/br/com/festagestor/model/Brinquedo.java
@@ -6,7 +6,6 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @DiscriminatorValue("Brinquedo")
--- src/main/java/br/com/festagestor/model/Item.java
+++ src/main/java/br/com/festagestor/model/Item.java
@@ -1,10 +1,7 @@
 package br.com.festagestor.model;
 
-import br.com.festagestor.dto.DadosAtualizacaoItem;
-import br.com.festagestor.dto.DadosCadastroItem;
+
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
